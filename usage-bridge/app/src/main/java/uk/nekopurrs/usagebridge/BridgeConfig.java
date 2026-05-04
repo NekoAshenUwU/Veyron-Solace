fc45c08d83ff15563f9263348a3a2c676a758122
CodeAndPurrs <tressho@gmail.com>
2026-05-04 15:52:57 +0800

    Add Usage Bridge config

---
+++ usage-bridge/app/src/main/java/uk/nekopurrs/usagebridge/BridgeConfig.java
@@ -0,0 +1,10 @@
+package uk.nekopurrs.usagebridge;
+
+public class BridgeConfig {
+    public static final long SYNC_INTERVAL_MS = 15L * 60L * 1000L;
+    public static final long DISPLAY_MIN_MS = 60L * 1000L;
+
+    private BridgeConfig() {
+        // No instances.
+    }
+}
